81ede70a8f21c87cb573c05e1f988aa8e3ae9d22
Flous108 <104693763+Flous108@users.noreply.github.com>
2026-02-03 01:21:39 +0100

    Update showhackpicrepeat.js

--- showhackpicrepeat.js
+++ showhackpicrepeat.js
@@ -1,6 +1,6 @@
 function showHackPic(){
 
-if (!img) {
+if (!img || typeof img == 'undefined') {
     let img = document.createElement('img');
 }
 img.src = 'https://m.media-amazon.com/images/I/31sDQI7yfDL._AC_UF894,1000_QL80_.jpg';
